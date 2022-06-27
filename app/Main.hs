063a87d37f43efacb54d7b11e184e961ca4fe55c
Khanh Hua <khanh.hua@doctari.de>
2022-06-27 22:58:34 +0200

    Simple server

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
+import Control.Monad
+import Network.Simple.TCP
+import Data.List (intersperse)
 import System.Directory
+import qualified Data.ByteString.Char8 as Char8
+import qualified Data.ByteString.UTF8 as UTF8
+import Data.Maybe
+
 
 data FileMeta =
   FileMeta
@@ -8,18 +15,38 @@ data FileMeta =
     , permissions :: Permissions
     , size :: Integer
     }
-  deriving (Show)
+
+instance Show FileMeta where
+  show (FileMeta name permissions size) = name <> ", " <> showPermissions permissions <> ", " <> show size
+
+showPermissions :: Permissions -> String
+showPermissions p = permissionsToFlag readable p 
+                  <> permissionsToFlag writable p 
+                  <> permissionsToFlag executable p
+                  <> permissionsToFlag searchable p
+  where
+    permissionsToFlag f = show . fromEnum . f
+
+showRows :: Show a => [a] -> String
+showRows xs = unwords . intersperse "\n" $ map show xs
 
 getFileMeta :: FilePath -> IO FileMeta
 getFileMeta p = FileMeta p <$> getPermissions p <*> getFileSize p
 
 showDir :: FilePath -> IO [FileMeta]
-showDir dir = do
-  ps <- listDirectory dir
-  mapM getFileMeta ps
+showDir dir = listDirectory dir >>= mapM getFileMeta
 
 catFile :: FilePath -> IO String
 catFile fname = undefined
 
 main :: IO ()
-main = undefined
+main = withSocketsDo $ do
+  serve (Host "127.0.0.1") "8000" accept
+  where
+    accept (socket, remoteAddr) = do
+      putStrLn $ "TCP connection established from " <> show remoteAddr
+      send socket $ UTF8.fromString "Welcome to Hunkemoeller"
+      forever $ do
+        received <- fromMaybe "" <$> recv socket 1024
+        putStrLn $ Char8.unpack received
+        send socket $ UTF8.fromString "OK\n"
